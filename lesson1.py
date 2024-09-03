1c6e4d429afd26667d6f9ff22b71f7dbe5172d26
Perez-Botello <olperez-bote@cps.k12.il.us>
2024-09-03 12:29:29 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+# print("Hello my name is Karol")
+#print statement allows us to print out a statement
+# this is the print statement
+#comments are not executed by the computer
+#comments are used to help us understand the code
+# print("I am a student at Hancock")
+# print("I am learning python")
+#create input for the user
+name= input("What is your name?")
+print("Hello,", name)
+#ask the user for their age
+#print out their age
+#ask the user for their favorite color
+#print out their favorite color
+#ask the user for their favorite food
+#print out their favorite food
+age= input("How old are you?")
+print("You are,", age)
+color= input("What is your favorite color?")
+print("Your favorite color is,", color)
+food= input("What is your favorite food?")
+print("Your favorite food is,", food)
+#concatenation is the process of combining strings with 
+#variables
+# print("You are" , age "years old")
+favorite_animal= input ("What's your favorite animal?")
+print("Your favorite animal is" , favorite_animal , "that's cool!" )
+#the plus sign is used to cocentrate strings
